0eff6f3102bcfbe50cbacac12c9978f323867ebc
白银良 <baiyinliang@126.com>
2020-07-15 18:11:38 +0800

    go语言的继承和复用

---
+++ src/ch12/extension/extension_test.go
@@ -0,0 +1,31 @@
+package extension
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p *Pet) Speak() {
+	fmt.Print("...")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+type Dog struct {
+	Pet
+}
+
+func (d *Dog) Speak() {
+	fmt.Println("Wang!")
+}
+
+func TestDog(t *testing.T) {
+	dog := new(Dog)
+	dog.SpeakTo("Chao")
+}
